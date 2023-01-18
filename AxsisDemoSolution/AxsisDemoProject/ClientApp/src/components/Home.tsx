fd1753968c81090306e92fc63c65206b1dd1a48d
José Daniel Flores Morales <dflores@ravisa.com>
2023-01-18 13:43:59 -0600

    InitialCommit for demo project

---
+++ AxsisDemoSolution/AxsisDemoProject/ClientApp/src/components/Home.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+const Home = () => (
+  <div>
+    <h1>Hello, world!</h1>
+    <p>This is a project developed for José Daniel Flores Morales, using:</p>
+    <ul>
+      <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
+      <li><a href='https://facebook.github.io/react/'>React</a> and <a href='https://redux.js.org/'>Redux</a> for client-side code</li>
+      <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+    </ul>
+    <p>This is just a demo project for Axsis Tecnologia company.</p>
+  </div>
+);
+
+export default connect()(Home);
