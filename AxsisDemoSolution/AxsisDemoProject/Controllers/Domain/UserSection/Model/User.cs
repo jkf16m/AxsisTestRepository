dd3d10bc8b3de0abbb6764842f53a155d9484b90
José Daniel Flores Morales <dflores@ravisa.com>
2023-01-20 01:45:40 -0600

    Made some private fields, static and readonly for the class User, to avoid reinstanciation of string.

--- AxsisDemoSolution/AxsisDemoProject/Controllers/Domain/UserSection/Model/User.cs
+++ AxsisDemoSolution/AxsisDemoProject/Controllers/Domain/UserSection/Model/User.cs
@@ -77,30 +77,30 @@ namespace AxsisDemoProject.Controllers.Domain.UserSection.Model
                 && CreationDate == other.CreationDate);
         }
 
-        public bool IsPasswordValid()
+        private static readonly string[] _regularExpressionsToMatch =
         {
-            var regularExpressionsToMatch = new string[]
-            {
                 ".{7,}",            // at least 7 characters
                 @"(?=.*\d)",        // at least has one number
                 @"(?=[a-z])",       // at least has one lowercase letter
                 @"(?=[A-Z])",       // at least has one capital letter
                 @"(?=[\!""\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\>\=\?\@\[\]\{\}\^\`\~\\_])"
-                                    // at least one symbol ^^^^^^
-
-            };
-            var allMatched = regularExpressionsToMatch
+                                    // at least one symbol
+        };
+        public bool IsPasswordValid()
+        {
+            
+            var allMatched = _regularExpressionsToMatch
                 .All(
-                expression => Regex.IsMatch(Password, expression)
+                    expression => Regex.IsMatch(Password, expression)
                 );
 
             return allMatched;
         }
 
+        private static readonly string _rfc5322CompliantEmailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
         public bool IsEmailValid()
         {
-            string Rfc5322CompliantEmailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-            return Regex.IsMatch(Email, Rfc5322CompliantEmailRegex);
+            return Regex.IsMatch(Email, _rfc5322CompliantEmailRegex);
         }
 
         /**
